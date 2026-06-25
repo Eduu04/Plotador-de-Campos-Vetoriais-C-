d3ee6a3fe097a68afead097adc5e14bf6a524aa0
Eduu04 <eduardoflorianidesouza@gmail.com>
2026-06-25 10:44:14 -0300

    Update ExpressaoMat.hpp

--- include/ExpressaoMat.hpp
+++ include/ExpressaoMat.hpp
@@ -1,8 +1,48 @@
 #pragma once
 
+/**
+ * @file ExpressaoMat.hpp
+ *
+ * @brief Declaração da classe ExpressaoMat.
+ *
+ */
+
+/**
+ * @class ExpressaoMat
+ *
+ * @brief Define uma interface para avaliação de expressões matemáticas.
+ *
+ * A classe estabelece uma interface comum para objetos capazes de representar e avaliar expressões matemáticas
+ * envolvendo duas variáveis. Ela fornece um método abstrato para cálculo do valor da expressão e permite que
+ * diferentes implementações utilizem estratégias distintas de avaliação, mantendo uma forma padronizada de acesso
+ * ao resultado.
+ *
+ */
 class ExpressaoMat{
     public:
+
+        /**
+         * @brief Detrói a expressão matemática.
+         *
+         * Destrutor virtual reponsável por garantir a liberação os recursos utilizados pelas classes derivadas
+         * quando manipuladas por meio de ponteiros.
+         *
+         */
         virtual ~ExpressaoMat() {}
+
+        /**
+         * @brief Avalia a expressão matemática.
+         *
+         * Método abstrato responsável por calcular o valor da expressão para os valores fornecidos das variáveis de entrada.
+         * Cada classe derivada deve implementar sua própria lógica de avaliação.
+         *
+         * @param a Valor da primeira variável utilizada no cálculo.
+         *
+         * @param b Valor da segunda variável utilizada no cálculo.
+         *
+         * @return Resultado numérico da expressão avaliada.
+         *
+         */
         virtual double avalia(double a, double b) = 0;
 
 };
