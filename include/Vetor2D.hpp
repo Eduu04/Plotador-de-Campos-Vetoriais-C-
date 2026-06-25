ba462fd9c999d4ca3c785dc840a9e750f9693ea3
Eduu04 <eduardoflorianidesouza@gmail.com>
2026-06-25 10:45:28 -0300

    Document Vetor2D class and its operations
    Added documentation for the Vetor2D class and its methods.

--- include/Vetor2D.hpp
+++ include/Vetor2D.hpp
@@ -1,13 +1,71 @@
 #pragma once
 #include "Ponto2D.hpp"
 
+/**
+ * @file Vetor2D.hpp
+ *
+ * @brief Declaração da classe Vetor2D.
+ *
+ */
+
+/**
+ * @class Vetor2D
+ *
+ * @brief Define uma estrutura genérica para representação de vetores bidimensionais.
+ * 
+ * A classe representa vetores no plano cartesiano por meio de duas componentes, uma horizontal e uma vertical.
+ * Ela herda da classe Ponto2D para reutilizar o armazenamento das coordenadas e fornece operações matemáticas básicas entre vetores
+ * como soma vetorial e multiplicação escalar.
+ * 
+ * @tparam T Tipo utilizado para armazenar as coordenadas do vetor.
+ *
+ */
 template <typename T>
 class Vetor2D : public Ponto2D<T>{
     public:
+        
+        /**
+         * @brief Constrói um vetor bidimensional.
+         *
+         * Inicializa as componentes horizontal e vertical do vetor utilizando os valores fornecidos.
+         * 
+         * @param _x Valor da coordenada horizontal.
+         * 
+         * @param _y Valor da coordenada vertical.
+         * 
+         */
         Vetor2D(T _x = 0, T _y = 0) : Ponto2D<T>(_x, _y) {};
+        
+        /**
+         * @brief Destrói o vetor bidimensional.
+         *
+         * Libera os recursos associados ao objeto.
+         *
+         */
         ~Vetor2D() = default;
-
+        
+        /** 
+         * @brief Realiza a soma entre dois vetores bidimensionais. 
+         *
+         * Executa a soma de componente a componente entre vetor atual e vetor fornecido.
+         *
+         * @param vec Vetor a ser somado.
+         * 
+         * @return Novo vetor contendo o resultado da soma.
+         * 
+         */
         Vetor2D<T> operator+(const Vetor2D<T>& vec) const;
+        
+        /**
+         * @brief Realiza a multiplicação de um vetor bidimensional.
+         *
+         * Multiplica cada componente do vetor pelo valor escalar fornecido.
+         *
+         * @param escalar Valor que multiplica o vetor.
+         * 
+         * @return Novo vetor contendo o resultado da soma.
+         * 
+         */
         Vetor2D<T> operator*(T escalar) const;
 };
 
@@ -20,4 +78,3 @@ template <typename T>
 Vetor2D<T> Vetor2D<T>::operator*(T escalar) const{
     return Vetor2D<T>(this->x*escalar, this->y*escalar);
 }
-
